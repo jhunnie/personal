03adbe42df3b59e11c4c8108b63a834eb9bbe2f1
Steven Peters <steve@Jennas-MacBook-Air.local>
2017-10-22 11:16:28 -0700

    Adding isLoggedIn middleware and profile route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var isLoggedIn = require('../middleware/isLoggedIn');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Nothing at the root route yet...');
+});
+
+router.get('/profile', isLoggedIn, function(req, res, next) {
+  res.send('You are logged in and this is your profile.');
 });
 
 module.exports = router;
